35054ca4e14d38125dce2a19f7a1840e790d50a9
Jeffrey D. Gordon <jeff@jdgwf.com>
2019-11-08 18:23:16 -0600

    Removed static binary in git repo

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,11 @@
 cd rust
 cargo build --release
+echo "---- timing rust release ./target/release/io_test ----"
 /usr/bin/time -v ./target/release/io_tests
 
 cd ../cpp
 make
+echo "---- timing c++ ./io_test ----"
 /usr/bin/time -v ./io_test
+echo "---- timing c++ ./io_test_static ----"
 /usr/bin/time -v ./io_test_static
