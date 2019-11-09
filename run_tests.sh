0534462903604393a153a0036c5be7b0437b2aef
Jeffrey D. Gordon <jeff@jdgwf.com>
2019-11-08 18:58:02 -0600

    Shell script fix

--- run_tests.sh
+++ run_tests.sh
@@ -10,5 +10,7 @@ echo "---- timing c++ ./io_test ----"
 echo "---- timing c++ ./io_test_static ----"
 /usr/bin/time -v ./io_test_static
 
+cd ..
+
 echo "---- effective differences between the two methods of generating files ----"
 diff -w  rust_outfile.txt cpp_outfile.txt
